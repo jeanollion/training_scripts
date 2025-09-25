84e3482bdb1463ec6e86ea7e47980c7388e4538c
jeanollion <smogollion@yahoo.fr>
2025-09-25 09:19:13 +0200

    bug fix in training_pixmclass.py @ export model. pixmclass: added parameter: min value for filters

--- training_scripts/training_pixmclass.py
+++ training_scripts/training_pixmclass.py
@@ -20,7 +20,7 @@ from pix_mclass.losses import get_class_weights, weighted_sparse_categorical_cro
 import pix_mclass.training as pmt
 from training_core import open_config_file, get_iterator, should_load_dataset_in_shm, get_shm_info
 
-__VERSION__ = "1.1.1"
+__VERSION__ = "1.1.2"
 
 parser = argparse.ArgumentParser()
 parser.add_argument("config_dir", type=str, help="directory containing the configuration file")
@@ -141,7 +141,7 @@ if __name__ == "__main__":
 
     if args.export_only:
         print(f"export only: init model with weights: {WEIGHT_PATH} (exist: {os.path.exists(WEIGHT_PATH)})")
-        model = init_model(args.class_number, **arch_conf)
+        model = init_model(args.class_number, arch_conf)
         assert os.path.exists(WEIGHT_PATH), f"weights {WEIGHT_PATH} not found"
         model.load_weights(WEIGHT_PATH)
         # export model
